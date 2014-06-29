103ae5c0cb7eccd3fcaec76932fbfb6fc061b15d
noisette.guillaume@gmail.com <noisette.guillaume@gmail.com@06c9b9ac-f734-c30b-1e1e-92eee771ba7c>
2014-06-29 00:16:24 +0000

    Correction merge

--- trunk/Projet Final/Physic_Engine/Physic_Engine/DestructorManager.cpp
+++ trunk/Projet Final/Physic_Engine/Physic_Engine/DestructorManager.cpp
@@ -16,14 +16,19 @@ DestructorManager::~DestructorManager(void)
 
 void DestructorManager::LaunchDestruction(ObjectManager & gom, Generic_Object * goImpact, vec3 positionImpact, vec3 positionLookAt)
 {
-	std::vector<unsigned short> * listIndexNeighbour = goImpact->RemoveVertexAround(vec3(0,0,0), 8.8f);
+	float radius = 30.0f;
+	float height = 20.0f;
+	int nbPointsCircle = 40;
+	int nbParticlesVoronoi = 5;
+
+	std::vector<unsigned short> * listIndexNeighbour = goImpact->RemoveVertexAround(vec3(0,0,0), radius * 1.1f);
 	goImpact->CalculateIndexes();
 	Generic_Object * go = new Generic_Object();
-	std::vector<unsigned short> * listVerticesConvexPolygon = DelaunayGenerator::LaunchCircleGeneration(8.0f, 40, go->verticesList, go->listEdges, go->listFaces);
-	ParaboloidSurfaceProjection::LaunchProjection(8.0f, 5.0f, go->verticesList, go->listEdges, go->listFaces);
+	std::vector<unsigned short> * listVerticesConvexPolygon = DelaunayGenerator::LaunchCircleGeneration(radius, nbPointsCircle, go->verticesList, go->listEdges, go->listFaces);
+	ParaboloidSurfaceProjection::LaunchProjection(radius, height, go->verticesList, go->listEdges, go->listFaces);
 	go->Scale(0.7f);
-	//VoronoiGenerator::GenerateVoronoi(gom, go, positionImpact, positionLookAt, vec3(0.0f, -2.5f, 0.0f), 8.0f, 5.0f, 0.7f, 5);
-	go->TranslateAndLookAtAndScale(positionImpact, positionLookAt, vec3(0.0f, -2.5f, 0.0f), 10.3f);
+	//VoronoiGenerator::GenerateVoronoi(gom, go, positionImpact, positionLookAt, vec3(0.0f, -height/2, 0.0f), radius, height, 0.7f, nbParticlesVoronoi);
+	go->TranslateAndLookAtAndScale(positionImpact, positionLookAt, vec3(0.0f, -height/2, 0.0f), 1.3f);
 	go->CalculateIndexes();
 	goImpact->Merge(positionImpact, go, listVerticesConvexPolygon, listIndexNeighbour);
 	delete listVerticesConvexPolygon;
